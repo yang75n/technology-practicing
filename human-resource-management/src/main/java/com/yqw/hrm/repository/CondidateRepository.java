12f7210919bbeccaa2a87146de5973c838b700ad
Yang Qiwen <yangqiwen@pachiratech.com>
2019-10-22 18:12:55 +0800

    update

--- human-resource-management/src/main/java/com/yqw/hrm/repository/CondidateRepository.java
+++ human-resource-management/src/main/java/com/yqw/hrm/repository/CondidateRepository.java
@@ -3,7 +3,6 @@ package com.yqw.hrm.repository;
 
 import com.yqw.hrm.domain.Condidate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CondidateRepository extends JpaRepository<Condidate, Integer> {
 }
